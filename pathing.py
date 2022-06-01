f4c60d09aebe458c0d2219b7a46809fa2caf7aa5
deonlillo <dlillo@calpoly.edu>
2022-06-01 01:06:17 -0700

    refactoring and make paths 2 blocks wide

--- pathing.py
+++ pathing.py
@@ -83,49 +83,81 @@ def place_structure(nbt_path: str, x: int, y: int, z: int, direction: Cardinals)
     print(block_loc)
     return block_loc
 
-if __name__ == "__main__":
-    blocks = place_structure("./villages/plains/houses/plains_big_house_1.nbt", 0, 0, 0, Cardinals.NORTH)
+def place_blocks_x(intermediate_block, start_block):
+    x = 0
+    z = 2
+    x_delt = intermediate_block[x] - start_block[x]
+    x_mag = abs(x_delt)
 
+    #place blocks in x direction
+    for i in range(x_mag - 1):
+        y_placement = heights[start_block[x] + (1 * (int)(x_delt / x_mag)) + (i * (int)(x_delt / x_mag))][start_block[z]] - 1
+        if INTF.getBlock(start_block[x] + (1 * (int)(x_delt / x_mag)) + (i * (int)(x_delt / x_mag)), y_placement, start_block[z]) == "minecraft:water":
+            INTF.placeBlock(start_block[x] + (1 * (int)(x_delt / x_mag)) + (i * (int)(x_delt / x_mag)), y_placement, start_block[z], "oak_planks")
+        else:
+            INTF.placeBlock(start_block[x] + (1 * (int)(x_delt / x_mag)) + (i * (int)(x_delt / x_mag)), y_placement, start_block[z], "grass_path")
+
+def place_blocks_z(intermediate_block, end_block):
+    x = 0
+    z = 2
+    z_delt = end_block[z] - intermediate_block[z]
+    z_mag = abs(z_delt)
+    
+    #place blocks in z direction
+    for j in range(z_mag - 1):
+        y_placement = heights[intermediate_block[x]][(intermediate_block[z] + (1 * (int)(z_delt / z_mag))) + (j * (int)(z_delt / z_mag))] - 1
+        if INTF.getBlock(intermediate_block[x], y_placement, (intermediate_block[z] + (1 * (int)(z_delt / z_mag))) + (j * (int)(z_delt / z_mag))) == "minecraft:water":
+            INTF.placeBlock(intermediate_block[x], y_placement, (intermediate_block[z] + (1 * (int)(z_delt / z_mag))) + (j * (int)(z_delt / z_mag)), "oak_planks")
+        else:
+            INTF.placeBlock(intermediate_block[x], y_placement, (intermediate_block[z] + (1 * (int)(z_delt / z_mag))) + (j * (int)(z_delt / z_mag)), "grass_path")
+        
+if __name__ == "__main__":
     heights = WORLDSLICE.heightmaps["MOTION_BLOCKING_NO_LEAVES"]
+    start_block = (68, heights[68][30], 30)
+    blocks = place_structure("./well.nbt", 0, 0, 0, Cardinals.NORTH)
+    
     print(STARTX, STARTY + 128, STARTZ)
 
     for d in blocks:
         x, y, z = d["location"]
-        INTF.placeBlock(STARTX + x, STARTY + y + 128, STARTZ + z, d["block"])
-
-    #for d in blocks:
-    #    x, y, z = d["location"]
-    #    INTF.placeBlock(6 + x, STARTY + y + 128, -7 + z, d["block"])
+        INTF.placeBlock(STARTX + x + start_block[0], STARTY + y + heights[start_block[0]][start_block[2]] - 9, STARTZ + z + start_block[2], d["block"])
 
     x = 0
     y = 1
     z = 2
-    #start_block = (11, 67, 32)
 
     mask = np.zeros(shape=heights.shape)
-
     start_block = (STARTX, heights[STARTX, STARTZ], STARTZ)
 
+    #initialize modifier    
     x_modifier = 1
     z_modifier = 1
+    #run in all 4 directions
     for l in range(4):
+        #l==0 is positive x direction
+        #negative z direction
         if l == 1:
             x_modifier = -1
+        #negative x direction
         if l == 2:
             x_modifier = -1
+        #positive z direction
         if l == 3:
             x_modifier = 1
+        #run pathing 5 times branching outwards
         for i in range(5):
+            #path lengths between 4 and 10 blocks
             x_distance = randint(4, 10)
             z_divergence_diff = randint(-x_distance, x_distance)
-
+            #set end block based on random values
             end_block_x = (start_block[x] + (x_distance * x_modifier))
             end_block_z = start_block[z] + z_divergence_diff
-
+            #if l is odd then start in z direction
             if l == 1 or l == 3:
                 end_block_x = start_block[x] + z_divergence_diff
                 end_block_z = (start_block[z] + (x_distance * x_modifier))
-
+            
+            #check to see if pathing end blocks will be too high up/down low
             breakCheck = False
             for j in range(100):
                 if (abs((heights[end_block_x][end_block_z]) - (heights[end_block_x][start_block[z]])) > 2 or abs((heights[end_block_x][start_block[z]]) - start_block[y]) > 2):
@@ -138,43 +170,46 @@ if __name__ == "__main__":
                         end_block_z = (start_block[z] + (x_distance * x_modifier))
                     if j == 99:
                         breakCheck = True
-
+            
+            #no suitable pathing available
             if breakCheck == True:
-                print("test")
                 break
-
+            
+            #set end block and intermediate block to go on height maps
             end_block = (end_block_x, heights[end_block_x][end_block_z], end_block_z)
             intermediate_block = (end_block[x], heights[end_block[x]][start_block[z]], start_block[z])
 
-            print(intermediate_block)
+            #place down the starting, ending, and intermediate blocks
+            INTF.placeBlock(start_block[x], heights[start_block[x]][start_block[z]] - 1, start_block[z], "grass_path")
+            INTF.placeBlock(end_block[x], heights[end_block[x]][end_block[z]] - 1, end_block[z], "grass_path")
+            INTF.placeBlock(intermediate_block[x], heights[intermediate_block[x]][intermediate_block[z]] - 1, intermediate_block[z], "grass_path")
 
-            INTF.placeBlock(start_block[x], heights[start_block[x]][start_block[z]] - 1, start_block[z], "gravel")
-            INTF.placeBlock(end_block[x], heights[end_block[x]][end_block[z]] - 1, end_block[z], "gravel")
-            INTF.placeBlock(intermediate_block[x], heights[intermediate_block[x]][intermediate_block[z]] - 1, intermediate_block[z], "gravel")
-            mask[start_block[x], start_block[z]] = 3
-            mask[end_block[x], end_block[z]] = 3
-            mask[intermediate_block[x], intermediate_block[z]] = 3
+            #placex
+            place_blocks_x(start_block, intermediate_block)
 
-            x_delt = intermediate_block[x] - start_block[x]
-            x_mag = abs(x_delt)
-            #place blocks in x direction
-            for i in range(x_mag - 1):
-                y_placement = heights[start_block[x] + (1 * (int)(x_delt / x_mag)) + (i * (int)(x_delt / x_mag))][start_block[z]] - 1
-                if INTF.getBlock(start_block[x] + (1 * (int)(x_delt / x_mag)) + (i * (int)(x_delt / x_mag)), y_placement, start_block[z]) == "minecraft:water":
-                    INTF.placeBlock(start_block[x] + (1 * (int)(x_delt / x_mag)) + (i * (int)(x_delt / x_mag)), y_placement, start_block[z], "oak_planks")
-                else:
-                    INTF.placeBlock(start_block[x] + (1 * (int)(x_delt / x_mag)) + (i * (int)(x_delt / x_mag)), y_placement, start_block[z], "gravel")
-                mask[start_block[x] + (1 * (int)(x_delt / x_mag)) + (i * (int)(x_delt / x_mag))][start_block[z]] = 3
-            z_delt = end_block[z] - intermediate_block[z]
-            z_mag = abs(z_delt)
-            #place blocks in z direction
-            for j in range(z_mag - 1):
-                y_placement = heights[intermediate_block[x]][(intermediate_block[z] + (1 * (int)(z_delt / z_mag))) + (j * (int)(z_delt / z_mag))] - 1
-                if INTF.getBlock(intermediate_block[x], y_placement, (intermediate_block[z] + (1 * (int)(z_delt / z_mag))) + (j * (int)(z_delt / z_mag))) == "minecraft:water":
-                    INTF.placeBlock(intermediate_block[x], y_placement, (intermediate_block[z] + (1 * (int)(z_delt / z_mag))) + (j * (int)(z_delt / z_mag)), "oak_planks")
-                else:
-                    INTF.placeBlock(intermediate_block[x], y_placement, (intermediate_block[z] + (1 * (int)(z_delt / z_mag))) + (j * (int)(z_delt / z_mag)), "gravel")
-                mask[intermediate_block[x]][(intermediate_block[z] + (1 * (int)(z_delt / z_mag))) + (j * (int)(z_delt / z_mag))] = 3
+            #placez
+            place_blocks_z(intermediate_block, end_block)
+
+            second_start = (start_block[x] + 1, heights[start_block[x]][start_block[z] + 1], start_block[z] + 1)
+            second_end = (end_block[x] + 1, heights[end_block[x]][end_block[z] + 1], end_block[z] + 1)
+            second_intermediate = (second_end[x], heights[second_end[x]][second_start[z]], second_start[z])
+            
+            #place down the starting, ending, and intermediate blocks
+            INTF.placeBlock(second_start[x], second_start[y] - 1, second_start[z], "grass_path")
+            INTF.placeBlock(second_end[x], second_end[y] - 1, second_end[z], "grass_path")
+            INTF.placeBlock(second_intermediate[x], second_intermediate[y] - 1, second_intermediate[z], "grass_path")
+            place_blocks_x(second_start, second_intermediate)
+
+            place_blocks_z(second_intermediate, second_end)
+
+            #temp
+            INTF.placeBlock(start_block[x], heights[start_block[x]][start_block[z]] - 1, start_block[z], "grass_path")
+            INTF.placeBlock(end_block[x], heights[end_block[x]][end_block[z]] - 1, end_block[z], "grass_path")
+            INTF.placeBlock(intermediate_block[x], heights[intermediate_block[x]][intermediate_block[z]] - 1, intermediate_block[z], "grass_path")
+
             print(heights)
+            #reset start block in the same cardinal direction
             start_block = end_block
-        start_block = (68, 70, 30)
+        
+        #reset start block for new cardinal direction
+        start_block = (68, heights[68][30], 30)
